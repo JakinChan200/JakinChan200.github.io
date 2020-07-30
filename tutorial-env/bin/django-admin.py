a52ebdab073d2c543b150c69cc7a3245dd0dcc8f
Natalie Suboc <nsuboc@gmail.com>
2020-07-30 16:56:17 -0700

    Setup Flask app and user account page

---
+++ tutorial-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nataliesuboc/Downloads/JakinChan200.github.io-master/tutorial-env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
