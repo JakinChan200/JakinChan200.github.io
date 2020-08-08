6d4006f3155b41ad5eab4e2f7a6ee7b3d7b72001
Natalie Suboc <nsuboc@gmail.com>
2020-08-08 16:11:17 -0700

    Add forms.py and config.py

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask
+from config import Config
+from forms import LoginForm
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__, template_folder="templates")
+SECRET_KEY = os.urandom(32)
+app.config['SECRET-KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///accounts.sqlite3'
 db = SQLAlchemy(app)
 db.Model.metadata.reflect(db.engine)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    form = LoginForm()
+    return render_template('index.html', title='Sign in', form=form)
 
 @app.route('/login-home')
 def login_home():
     return render_template('login-home.html')
-
-if __name__ == '__main__':
-    app.run()
-
